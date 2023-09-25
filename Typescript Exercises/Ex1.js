7bf54fbd96d290b73ba98348dc44a7b2eb7647e6
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2023-09-25 14:55:56 -0500

    random update

---
+++ Typescript Exercises/Ex1.js
@@ -0,0 +1,7 @@
+//Declare a variable studentName of type string and assign it your name.
+var studentName;
+studentName = 'Garrett';
+//Declare a variable totalCourses of type number and assign it the number of courses you are taking.
+var totalCourses;
+totalCourses = 3;
+console.log(totalCourses, studentName);
